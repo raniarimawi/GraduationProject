de2931fb1f86300d78ba70de873014af2cb50e1a
Rania Rimawi <raniarimawi1@gmail.com>
2025-07-03 19:08:23 +0300

    load

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.graduationproject.skindiseasedetection',
-  appName: 'SKIN DISEASE DETECTION',
-  webDir: 'dist'
-};
-
-export default config;
